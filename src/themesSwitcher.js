764c0c962493a711cafec3df54b636992aa8a2bc
Vladislav Kulachko <vladcoool@gmail.com>
2021-06-17 23:39:17 +0300

    продакшн билд

--- src/themesSwitcher.js
+++ src/themesSwitcher.js
@@ -1,7 +1,23 @@
 const switchThemes = document.querySelector('#theme-switch-toggle');
 const body = document.querySelector('body');
-switchThemes.addEventListener('change', () => {
-  if (body.classList.contains('light-theme')) {
-    body.classList.replace('light-theme', 'dark-theme');
-  } else body.classList.replace('dark-theme', 'light-theme');
+
+document.getElementById('theme-switch-toggle').setAttribute('checked', true);
+body.classList.add('dark-theme');
+
+switchThemes.addEventListener('change', e => {
+  const checkState = e.currentTarget.checked;
+
+  localStorage.setItem('defaultTheme', checkState);
+
+  if (!checkState) {
+    body.classList.replace('dark-theme', 'light-theme');
+  } else body.classList.replace('light-theme', 'dark-theme');
 });
+
+function onCurrentTheme() {
+  const defaultTheme = localStorage.getItem('defaultTheme');
+  if (defaultTheme === 'true') return;
+  body.classList.replace('dark-theme', 'light-theme');
+  document.getElementById('theme-switch-toggle').removeAttribute('checked');
+}
+onCurrentTheme();
